a7e06c69b82b9ed43f23e9556312d2f738664a88
GitGautamHub <gautamk8760@gmail.com>
2024-01-07 18:28:36 +0530

    more on js

--- 03tailwindprops/src/App.js
+++ 03tailwindprops/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Card from './Card.jsx'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>tailwind test</h1>
+      <br />
+      <Card username="gautam" btntext="clik me" />
+      <Card username="manya"  />
+    </>
   );
 }
 
